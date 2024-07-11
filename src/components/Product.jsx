02260f05d14b4b822ee7c653244c89cf9d5c7925
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-11 13:26:20 +0100

    Enhancements on cart control and implementing cart functionality and quantity control In this commit i enhanced the Product component by addition of intuitive quantity controls and cart interaction features improving usability

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,87 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import product1 from '../assets/slider1.jpg'
+import product1 from '../assets/slider1.jpg';
 import '../css/product.css';
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Product = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAddQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleSubtractQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+    alert(`Added ${quantity} items to cart`);
+  };
 
   return (
     <>
-      <div>
-        <Header/>
-        <Navbar/>
-        <section id="product-info">
-          <div className="item-image-parent">
-            <div className="item-list-vertical">
-              <div className="thumb-box">
-                <img src={product1} alt="thumbnail" />
+      <Header />
+      <Navbar />
+      <div className="container mt-5">
+        <section id="product-info" className="row">
+          <div className="col-md-6">
+            <div className="item-image-parent">
+              <div className="item-list-vertical d-flex flex-column">
+                <div className="thumb-box mb-2">
+                  <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                </div>
+                <div className="thumb-box mb-2">
+                  <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                </div>
+                <div className="thumb-box mb-2">
+                  <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                </div>
               </div>
-              <div className="thumb-box">
-                <img src={product1} alt="thumbnail" />
+              <div className="item-image-main">
+                <img src={product1} alt="default" className="img-fluid" />
               </div>
-              <div className="thumb-box">
-                <img src={product1} alt="thumbnail" />
-              </div>
-            </div>
-            <div className="item-image-main">
-              <img src={product1} alt="default" />
             </div>
           </div>
 
-          <div className="item-info-parent">
-            <div className="main-info">
-              <h4>EYEBOGLER Regular Fit Men's Cotton T-Shirt</h4>
-              <div className="star-rating">
-                <span>★★★★</span>★
+          <div className="col-md-6">
+            <div className="item-info-parent">
+              <div className="main-info">
+                <h4>Gaming Headset for PS4, PS5, PC, Xbox One, Over-Ear Gaming Headphones with Noise Cancelling Mic, Premium Stereo, Lightweight Comfortable Earmuffs for Switch Laptop Mobile, Red (GM-1)</h4>
+                <div className="star-rating">
+                  <span>★★★★</span>★
+                </div>
+                <p>Price: <span id="price">$ 449.00</span></p>
               </div>
-              <p>Price: <span id="price">$ 449.00</span></p>
-            </div>
-            <div className="select-items">
-              <div className="change-color">
-                <label><b>Colour:</b> Black</label><br />
-                <div className="thumb-box">
-                  <img src={product1} alt="thumbnail" />
+              <div className="select-items">
+                <div className="change-color mb-3">
+                  <label><b>Colour:</b> Black</label><br />
+                  <div className="d-flex">
+                    <div className="thumb-box mr-2">
+                      <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                    </div>
+                    <div className="thumb-box mr-2">
+                      <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                    </div>
+                    <div className="thumb-box">
+                      <img src={product1} alt="thumbnail" className="img-thumbnail" />
+                    </div>
+                  </div>
                 </div>
-                <div className="thumb-box">
-                  <img src={product1} alt="thumbnail" />
-                </div>
-                <div className="thumb-box">
-                  <img src={product1} alt="thumbnail" />
-                </div>
-                {/* Repeat for other colors */}
-              </div>
 
-              <div className="change-size">
-                <label><b>Size:</b></label><br />
-                <select>
-                  <option>S</option>
-                  <option>M</option>
-                  <option>L</option>
-                  <option>XL</option>
-                  <option>2XL</option>
-                </select>
-              </div>
+                <div className="change-size mb-3">
+                  <label><b>Size:</b></label><br />
+                  <select className="form-control w-50">
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                    <option>2XL</option>
+                  </select>
+                </div>
 
-              <div className="description">
-                <ul>
-                  <li>Care Instructions: Machine Wash</li>
-                  <li>Fit Type: Classic Fit</li>
-                  <li>Color name: Black-White</li>
-                  <li>Material: Cotton</li>
-                  <li>Pattern: Solid</li>
-                </ul>
+                <div className="description mb-3">
+                  <ul>
+                    <li>Care Instructions: Machine Wash</li>
+                    <li>Fit Type: Classic Fit</li>
+                    <li>Color name: Black-White</li>
+                    <li>Material: Cotton</li>
+                    <li>Pattern: Solid</li>
+                  </ul>
+                </div>
+
+                <div className="add-to-cart">
+                  <label><b>Quantity:</b></label><br />
+                  <div className="input-group w-50 mb-3">
+                    <div className="input-group-prepend">
+                      <button className="btn btn-outline-secondary" type="button" onClick={handleSubtractQuantity}>-</button>
+                    </div>
+                    <input type="text" className="form-control text-center" value={quantity} readOnly />
+                    <div className="input-group-append">
+                      <button className="btn btn-outline-secondary" type="button" onClick={handleAddQuantity}>+</button>
+                    </div>
+                  </div>
+                  <button onClick={handleAddToCart} className="btn btn-warning btn-block">Add to Cart</button>
+                </div>
               </div>
             </div>
           </div>
         </section>
-        <Footer/>
       </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Product;
