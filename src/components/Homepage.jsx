02f4b8efc81452e54812b790cca169c582fea1f0
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-12 21:12:29 +0100

    Integration of the core components to the homepage This commit updates the main layout for the homepage, by integrating the major componets ,this will enhance the user interface.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Header from './Header'
-import Navbar from './Navbar'
-import Carousel from './Carousel'
-import Cards from './Cards'
-import ShopItemsSlider from './Sliders'
-import Footer from './Footer'
+import React from 'react';
+import Header from './Header';
+import Navbar from './Navbar';
+import Carousel from './Carousel';
+import Cards from './Cards';
+import ShopItemsSlider from './Sliders';
+import Footer from './Footer';
 
-const Homepage = () => {
+const Homepage = ({ cartCount }) => {
   return (
     <div>
-      <Header/>
-      <Navbar/>
-      <Carousel/>
-      <Cards/>
-      <ShopItemsSlider/>
-      <Footer/>
+      <Header cartCount={cartCount} />
+      <Navbar />
+      <Carousel />
+      <Cards />
+      <ShopItemsSlider />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
