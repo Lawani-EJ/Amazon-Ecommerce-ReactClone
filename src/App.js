82a76ed6fae39969762ab0bd7586a411fdad477d
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-16 15:26:59 +0100

    Additions of the cart state management and also addition of the Merchantregister route For this commit i introduced the cartcount state into the app component this will help keep the number of items in the cart Also updates on the routes in the app component have been added Enabling good navigation.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Product from './components/Product';
-import Merchant from './components/Merchant'; 
-import MerchantSignup from './components/MerchantSignup'; 
+import Merchant from './components/Merchant';
+import MerchantSignup from './components/MerchantSignup';
+import MerchantRegister from './components/MerchantRegister';
 
 const App = () => {
+  const [cartCount, setCartCount] = useState(0);
+
+  const handleAddToCart = (quantity) => {
+    setCartCount(cartCount + quantity);
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Homepage cartCount={cartCount} />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="/merchant" element={<Merchant />} /> 
-      <Route path="/merchantsignup" element={<MerchantSignup />} /> 
+      <Route path="/product/:id" element={<Product onAddToCart={handleAddToCart} cartCount={cartCount} />} />
+      <Route path="/merchant" element={<Merchant />} />
+      <Route path="/merchantsignup" element={<MerchantSignup />} />
+      <Route path='/merchantregister' element={<MerchantRegister/>}/>
     </Routes>
   );
 };
