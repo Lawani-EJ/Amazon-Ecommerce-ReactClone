7a09ca503840dc968d74f96f28f41f53531d56cc
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-23 20:33:40 +0100

    Created category Component

---
+++ src/components/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Navbar from './Navbar'
+
+const Category = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      
+    </div>
+  )
+}
+
+export default Category
